a149f63d44a626b6ff071ce2ac4d3d82c2540325
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-01 18:07:18 +0200

    Initial commit

---
+++ Src/MouseTrackerOverlay/MouseTrackerOverlay/Form1.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using WebView2 = Microsoft.Web.WebView2.WinForms.WebView2;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MouseHooksAPI;
+
+namespace MouseTrackerOverlay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        private static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        private static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        private static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        private static uint CurrentResolution = 0;
+        private static bool running;
+        private MouseHooks mh = new MouseHooks();
+        private static int x, y, mousex, mousey;
+        private static bool mouseclick;
+        private WebView2 webView21 = new WebView2();
+        private static int width = Screen.PrimaryScreen.Bounds.Width;
+        private static int height = Screen.PrimaryScreen.Bounds.Height;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeBeginPeriod(1);
+            NtSetTimerResolution(1, true, ref CurrentResolution);
+            Task.Run(() => Start());
+        }
+        private async void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Size = new Size(width, height);
+            this.Location = new Point(0, 0);
+            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--disable-web-security --allow-file-access-from-files --allow-file-access", "en");
+            CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
+            await webView21.EnsureCoreWebView2Async(environment);
+            webView21.CoreWebView2.SetVirtualHostNameToFolderMapping("appassets", "assets", CoreWebView2HostResourceAccessKind.DenyCors);
+            webView21.CoreWebView2.Settings.AreDevToolsEnabled = false;
+            webView21.KeyDown += WebView21_KeyDown;
+            webView21.Source = new Uri("https://appassets/index.html");
+            webView21.Dock = DockStyle.Fill;
+            webView21.DefaultBackgroundColor = Color.Transparent;
+            this.Controls.Add(webView21);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            OnKeyDown(keyData);
+            return true;
+        }
+        private void WebView21_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        private void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void Start()
+        {
+            running = true;
+            mh.Scan();
+            mh.BeginPolling();
+            Task.Run(() => task());
+        }
+        private void task()
+        {
+            for (; ; )
+            {
+                if (!running)
+                    break;
+                try
+                {
+                    mousex = mh.MouseX;
+                    mousey = mh.MouseY;
+                    mouseclick = mh.MouseLeftButton;
+                }
+                catch { }
+                Thread.Sleep(1);
+            }
+        }
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                await execScriptHelper($"setMouse('{mousex.ToString()}', '{mousey.ToString()}', '{mouseclick.ToString()}');");
+            }
+            catch { }
+        }
+        private async Task<String> execScriptHelper(String script)
+        {
+            var x = await webView21.ExecuteScriptAsync(script).ConfigureAwait(false);
+            return x;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            running = false;
+            Thread.Sleep(100);
+            mh.Close();
+            webView21.Dispose();
+        }
+    }
+}
